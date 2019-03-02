01ab9cf4be00c2a7afd2a0433acaef8628664161
eguido52 <eguido@csumb.edu>
2019-03-02 15:26:11 -0800

    first commit

---
+++ instagram_clone/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  instagram_clone
+//
+//  Created by Emmanuel Guido on 3/1/19.
+//  Copyright © 2019 eguido. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
